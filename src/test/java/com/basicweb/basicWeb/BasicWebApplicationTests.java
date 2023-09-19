c3de028150967897b2e8e6ff6aba3589a4d64048
vikesh107 <vikesh.gupta@nagarro.com>
2023-09-19 11:15:13 +0530

    this is after chnagengs is test class

--- src/test/java/com/basicweb/basicWeb/BasicWebApplicationTests.java
+++ src/test/java/com/basicweb/basicWeb/BasicWebApplicationTests.java
@@ -1,13 +1,10 @@
 package com.basicweb.basicWeb;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BasicWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
